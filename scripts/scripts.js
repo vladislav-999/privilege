e0bc37ae2341cebeea2ae7638c050d28e20e707f
Vladislav Hlimbotskyi <vladislav.olekseevich18@gmail.com>
2017-09-13 18:48:03 +0300

    Catalog Page apend scripts

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -8,4 +8,24 @@ $(document).ready(function(){
         items: 1,
     });
 
+    $('a.product-sort-view__a').click(function(){
+        if(!$(this).hasClass('active')){
+            $('a.product-sort-view__a').removeClass('active');
+            $(this).addClass('active');
+            $('.product-list').removeClass('grid');
+            $('.product-list').removeClass('list');
+
+            $('.product-list').addClass($(this).attr('data-view'));
+        }
+    });
+
+    $('.hide__toggle').click(function(){
+        var that = $(this).parent().parent();
+        if(that.hasClass('hide')){
+            that.removeClass('hide');
+        }else{
+            that.addClass('hide');
+        }
+    });
+
 });
